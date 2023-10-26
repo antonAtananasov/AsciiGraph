ab76ddfbefe62aa371a58647678d23e7e296c5b2
antonAtananasov <anton_atananasov@abv.bg>
2023-10-26 08:46:16 +0300

    added parameters to test function for better calling from outside script

--- run.py
+++ run.py
@@ -1,21 +1,24 @@
 from AsciiPlotter import *
 
-scale = 1
+SCALE = 1  # reduce this to change resolution fast
+NATIVE_RESOLUTION = (231, 115)
+
+# =========================================
 
 
-def test():
+def test(nativeSize: tuple, scale: float) -> None:
     """
     Description
     -----------
     Observe
     """
-    plotter = AsciiPlotter(
-        (np.int_(np.array((231, 115)) / scale))
-    )  # ((231, 115))  # (canvasSize=(9, 19))
+    plotter10x10 = AsciiPlotter(
+        (np.int_(np.array(nativeSize) * scale))
+    )  # (canvasSize=(9, 19))
 
-    print(f"ASCII rsolution {plotter.canvasSize[0]}x{plotter.canvasSize[1]}")
-    print(f"x in range [{plotter.bounds[0][0]},{plotter.bounds[0][1]}]")
-    print(f"y in range [{plotter.bounds[1][0]},{plotter.bounds[1][1]}]")
+    print(f"ASCII rsolution {plotter10x10.canvasSize[0]}x{plotter10x10.canvasSize[1]}")
+    print(f"x in range [{plotter10x10.bounds[0][0]},{plotter10x10.bounds[0][1]}]")
+    print(f"y in range [{plotter10x10.bounds[1][0]},{plotter10x10.bounds[1][1]}]")
     print("")
     eqs = [
         "y^2+x^2<=10^2",
@@ -26,10 +29,10 @@ def test():
         # r"y=x^x",
     ]
     colors = [
-        plotter.colors.RED,
-        plotter.colors.BLUE,
-        plotter.colors.YELLOW,
-        plotter.colors.GREEN,
+        ConsoleColors.RED,
+        ConsoleColors.BLUE,
+        ConsoleColors.YELLOW,
+        ConsoleColors.GREEN,
     ]
 
     # individual===============================================
@@ -37,7 +40,7 @@ def test():
         eq = eqs[i]
         color = colors[i % len(colors)]
         print("", tex2py(eq))
-        print(plotter.plotCartesianAsciiEquations([eq], colors=[color]))
+        print(plotter10x10.plotCartesianAsciiEquations([eq], colors=[color]))
 
     # # overlay===============================================
     # plot = plotter.plotCartesianAsciiEquations(
@@ -56,23 +59,23 @@ def test():
     # system===============================================
     for eq in eqs:
         print("|", tex2py(eq))
-    plot = plotter.plotCartesianAsciiEquations(eqs, system=True, colors=colors)
+    plot = plotter10x10.plotCartesianAsciiEquations(eqs, system=True, colors=colors)
     print(plot)
     # cartesian points===============================================
     pts = [
         (0, 0),
         (4, 6),
         (-2, 3),
-        (plotter.bounds[0][0] + 3, plotter.bounds[1][0] + 3),
+        (plotter10x10.bounds[0][0] + 3, plotter10x10.bounds[1][0] + 3),
         (
-            random.randrange(plotter.bounds[0][0], plotter.bounds[0][1]),
-            random.randrange(plotter.bounds[1][0], plotter.bounds[1][1]),
+            random.randrange(plotter10x10.bounds[0][0], plotter10x10.bounds[0][1]),
+            random.randrange(plotter10x10.bounds[1][0], plotter10x10.bounds[1][1]),
         ),
     ]
     chars = ["O", "A", "B", "C", "R"]
     for i in range(len(pts)):
         print(":", f"{chars[i]}{pts[i]}")
-    print(plotter.plotCartesianAsciiPoints(pts, chars, colors=colors))
+    print(plotter10x10.plotCartesianAsciiPoints(pts, chars, colors=colors))
 
     # polar=================================================
     eqs = [
@@ -87,20 +90,20 @@ def test():
     for i in range(len(eqs)):
         eq = eqs[i]
         print(":", tex2py(eq))
-    polarPlot = plotter.plotPolarAsciiEquations(
+    polarPlot = plotter10x10.plotPolarAsciiEquations(
         eqs,
         colors=[
-            plotter.colors.RED,
-            # plotter.colors.RED,
-            # plotter.colors.RED,
-            # plotter.colors.RED,
-            # plotter.colors.RED,
-            # plotter.colors.RED,
-            plotter.colors.BLUE,
-            plotter.colors.YELLOW,
-            plotter.colors.GREEN,
-            plotter.colors.BEIGE,
-            plotter.colors.GREEN2,
+            ConsoleColors.RED,
+            # ConsoleColors.RED,
+            # ConsoleColors.RED,
+            # ConsoleColors.RED,
+            # ConsoleColors.RED,
+            # ConsoleColors.RED,
+            ConsoleColors.BLUE,
+            ConsoleColors.YELLOW,
+            ConsoleColors.GREEN,
+            ConsoleColors.BEIGE,
+            ConsoleColors.GREEN2,
         ],
     )
     print(polarPlot)
@@ -108,22 +111,19 @@ def test():
     for i in range(len(eqs)):
         eq = eqs[i]
         print("|", tex2py(eq))
-    polarPlot = plotter.plotPolarAsciiEquations(eqs, system=True, contourOnTop=True)
+    polarPlot = plotter10x10.plotPolarAsciiEquations(
+        eqs, system=True, contourOnTop=True
+    )
     print(polarPlot)
     # polar=================================================
     pts = [(2, 30), (4, 150), (10, -150)]
     for pt in pts:
         print(";", pt)
-    print(plotter.plotPolarAsciiPoints(pts, colors=colors))
+    print(plotter10x10.plotPolarAsciiPoints(pts, colors=colors))
 
-
-if __name__ == "__main__":
-    import time
-
-    start_time = time.time_ns()
-    test()
-
-    plotter = AsciiPlotter((np.int_(np.array((231, 115)) / scale)), ((-7, 7), (-7, 7)))
+    plotter7x7 = AsciiPlotter(
+        (np.int_(np.array(nativeSize) * scale)), ((-7, 7), (-7, 7))
+    )
     eqs = [
         r"((x/7)^2*sqrt(abs(abs(x)-3)/(abs(x)-3))+(y/3)^2*sqrt(abs(y+3/7*sqrt(33))/(y+3/7*sqrt(33)))-1)=0",
         r"(abs(x/2)-((3*sqrt(33)-7)/112)*x^2-3+sqrt(1-(abs(abs(x)-2)-1)^2)-y)=0",
@@ -134,6 +134,13 @@ if __name__ == "__main__":
     ]
     for i in range(len(eqs)):
         print(":", eqs[i])
+    print(plotter7x7.plotCartesianAsciiEquations(eqs, intersect=None))
 
-    print(plotter.plotCartesianAsciiEquations(eqs, intersect=None))
+
+if __name__ == "__main__":
+    import time
+
+    start_time = time.time_ns()
+    test(NATIVE_RESOLUTION, SCALE)
+
     print("Done in", (time.time_ns() - start_time) / 1e9, "s")
